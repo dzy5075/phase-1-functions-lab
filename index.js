22cf415cb4277c959a465c9d81a33fae08e4bf6a
Daniel Yim <daniel.yim96@gmail.com>
2022-11-21 23:52:53 -0500

    commit tree 48c87c670ec6a0f2a56b5827a4ac3cf7ca308b79

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    const blocks = Math.abs(someValue - 42);
+    return blocks
+  }
+
+function distanceFromHqInFeet(someValue) {
+     distanceFromHqInBlocks(someValue); 
+        const blocks = Math.abs(someValue - 42)
+        const feet = blocks * 264
+    return feet
+}
+function distanceTravelledInFeet(start, destination) {
+    const feets = (Math.abs(start - destination) * 264)
+return feets
+}
+function calculatesFarePrice(start, destination) {
+    let distance = distanceTravelledInFeet(start, destination);
+    if (distance <=400 )
+        {return 0}
+    else if (distance >= 400 && distance <= 2000)
+        {return ((distance - 400) * 0.02)}
+    else if (distance > 2000 && distance  <= 2500)
+        {return 25}
+    else if (distance > 2500)
+        {return 'cannot travel that far'}
+}
